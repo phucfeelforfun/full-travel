c08fd9ce08c7c0088168427779321f613a9ab4a6
phucfeelforfun <banbmt1509@gmail.com>
2023-08-29 16:50:37 +0700

    update

--- assets/js/login.js
+++ assets/js/login.js
@@ -207,6 +207,9 @@ function Validator2(option) {
 
                 return value === formElement.querySelector(selector).value ? undefined : 'Mật khẩu nhập lại không chính xác'
             }
+        },
+        hasToChecked: function (attribute) {
+            return attribute === 'checked' ? undefined : 'Vui lòng chọn ít nhất một tùy chọn'
         }
     }
 
@@ -218,22 +221,24 @@ function Validator2(option) {
             var rules = input.getAttribute('rules').split('|')
 
             for (var rule of rules) {
-                var ruleInfo;
-                var isRuleHasValue = rule.includes(':');
+                var ruleFunc = validatorRules[rule]
+                var isRuleHasValue = rule.includes(':')
 
                 if (isRuleHasValue) {
-                    ruleInfo = rule.split(':');
-                    rule = ruleInfo[0];
-                }
-                var ruleFunc = validatorRules[rule];
+                    var ruleInfo = rule.split(':')
 
-                if (isRuleHasValue) {
+                    ruleFunc = validatorRules[ruleInfo[0]]
                     ruleFunc = ruleFunc(ruleInfo[1])
                 }
+                
+
+                
 
                 if (Array.isArray(formRules[input.name])) {
-
-                    formRules[input.name].push(ruleFunc)
+                    if (formRules[input.name].every(func => func !== ruleFunc)){
+                        formRules[input.name].push(ruleFunc)
+                    }
+                    
                 }
                 else {
                     formRules[input.name] = [ruleFunc];
@@ -280,7 +285,7 @@ function Validator2(option) {
             }
 
         }
-        return !errorMessage;
+        return errorMessage;
     }
     function handleClearError(event) {
         var formGroup = getParent(event.target, '.form-group');
@@ -303,41 +308,37 @@ function Validator2(option) {
         for (var [index, input] of inputs.entries()) {
 
             if (input.type === 'checkbox' || input.type === 'radio') {
-
+                
                 if (index > 0 && isValid === true && input.name === inputs[--index].name) {
-                    console.log('oke')
+                    
                     continue
                 }
                 if (!handleValidate({ target: input })) {
-                    isValid = false;
-                    handleClearError({ target: input })
+                   isValid = true;
+                    handleClearError({ target: input });
+                    
+                    
                 }
             }
-
-            if (handleValidate({ target: input })) {
+                if (handleValidate({ target: input })) {
                 isValid = false
             }
+            
             ;
-        }
-
+        };
+        // console.log(isValid);
         if (isValid) {
-
             var enable = formElement.querySelectorAll('[name]')
             var formValue = Array.from(enable).reduce(function (value, input) {
                 switch (input.type) {
                     case 'radio':
-                        value[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked').value;
-                        break;
+                        if (input.matches(':checked')) value[input.name] = input.value
+                        break
                     case 'checkbox':
-                        if (!input.matches(':checked')) return value;
-                        // {
-                        //     value[input.name]='';
-                        //     return value
-                        // } 
                         if (!Array.isArray(value[input.name])) {
                             value[input.name] = [];
                         }
-                        value[input.name].push(input.value);
+                        if (input.matches(':checked')) value[input.name].push(input.value);
                         break;
                     case 'file':
                         value[input.name] = input.files;
@@ -347,196 +348,10 @@ function Validator2(option) {
                 }
                 return value;
             }, {});
-
+            
             console.log(formValue)
 
         }
     }
 }
 
-// function Validator2(formSelector) {
-
-//     var _this = this;
-//     function getParent(element, selector) {
-
-//         while (element.parentElement) {
-//             if (element.parentElement.matches(selector)) {
-//                 return element.parentElement
-//             }
-//             element = element.parentElement
-//         }
-
-//     }
-
-//     /**
-//      * Rules:
-//      * - Error: return `error message`
-//      * - Non-error: return `undefined`
-//      */
-//     var formRules = {}
-//     var validatorRules = {
-//         required: function (value) {
-//             return value ? undefined : 'Vui lòng nhập trường này'
-//         },
-//         email: function (value) {
-//             var regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-//             return regex.test(value) ? undefined : 'Vui lòng nhập email'
-//         },
-//         min: function (min) {
-//             return function (value) {
-//                 return value.length >= min ? undefined : `Vui lòng nhập tối thiếu ${min} ký tự`
-//             }
-//         },
-//         max: function (max) {
-//             return function (value) {
-//                 return value.length <= max ? undefined : `Số ký tự tối đa là ${min}`
-//             }
-//         },
-//         hasToChecked: function (attribute) {
-//             return attribute === 'checked' ? undefined : 'Vui lòng chọn ít nhất một tùy chọn'
-//         }
-//     }
-
-//     // Take form element from DOM by `formSelector`
-//     var formElement = document.querySelector(formSelector);
-
-//     if (formElement) {
-
-//         var inputs = formElement.querySelectorAll('[name][rules]')
-//         for (var input of inputs) {
-
-//             var rules = input.getAttribute('rules').split('|')
-
-//             for (rule of rules) {
-
-//                 var ruleFunc = validatorRules[rule]
-//                 var isRuleHasValue = rule.includes(':')
-
-//                 if (isRuleHasValue) {
-//                     var ruleInfo = rule.split(':')
-
-//                     ruleFunc = validatorRules[ruleInfo[0]]
-//                     ruleFunc = ruleFunc(ruleInfo[1])
-//                 }
-
-
-//                 if (Array.isArray(formRules[input.name])) {
-//                     if (formRules[input.name].every(func => func !== ruleFunc)) {
-//                         formRules[input.name].push(ruleFunc)
-//                     }
-//                 } else {
-//                     formRules[input.name] = [ruleFunc]
-//                 }
-//             }
-
-//             // Events listen onchange
-
-//             input.onblur = handleValidate
-//             input.oninput = handleClearError
-//         }
-
-//         // Validation function
-//         function handleValidate(e) {
-//             var rules = formRules[e.target.name]
-//             var errorMessage
-//             for (var rule of rules) {
-//                 switch (e.target.type) {
-//                     case 'checkbox':
-//                     case 'radio':
-//                         errorMessage = rule(e.target.checked);
-//                         break
-//                     default:
-//                         errorMessage = rule(e.target.value)
-//                 }
-//                 if (errorMessage) break
-
-//             }
-
-//             // Send error message when error is found
-//             if (errorMessage) {
-//                 var formGroup = getParent(e.target, '.form-group')
-//                 if (formGroup) {
-//                     formGroup.classList.add('invalid')
-//                     var formMessage = formGroup.querySelector('.form-message')
-//                     if (formMessage) {
-//                         formMessage.innerText = errorMessage
-//                     }
-//                 }
-//             }
-
-//             return errorMessage
-//         }
-
-//         // Clear error message function
-//         function handleClearError(e) {
-//             var formGroup = getParent(e.target, '.form-group')
-//             if (formGroup.classList.contains('invalid')) {
-//                 formGroup.classList.remove('invalid')
-//             }
-
-//             var formMessage = formGroup.querySelector('.form-message')
-//             if (formMessage) {
-//                 formMessage.innerText = ''
-//             }
-//         }
-//     }
-
-//     // Handle submit form action
-//     formElement.onsubmit = function (e) {
-//         e.preventDefault()
-//         var isValid = true
-//         var inputs = formElement.querySelectorAll('[name][rules]')
-//         for (var [index, input] of inputs.entries()) {
-
-//             if (input.type === 'checkbox' || input.type === 'radio') {
-                
-//                 if (index > 0 && isValid === true && input.name === inputs[--index].name) {
-//                     continue
-//                 }
-//                 if (!handleValidate({ target: input })) {
-//                     isValid = true
-//                     handleClearError({ target: input })
-//                 }
-//             }
-            
-//             if (handleValidate({ target: input })) {
-//                 isValid = false
-//             }
-//         }
-
-
-//         // Submit when form is valid
-//         if (isValid) {
-//             if (typeof _this.onSubmit === 'function') {
-
-//                 var enableInputs = formElement.querySelectorAll('[name]')
-
-//                 var formValues = Array.from(enableInputs).reduce(function (values, input) {
-
-//                     switch (input.type) {
-//                         case 'checkbox':
-//                             if (!Array.isArray(values[input.name])) values[input.name] = []
-//                             if (input.matches(':checked')) values[input.name].push(input.value)
-//                             break
-//                         case 'radio':
-//                             if (input.matches(':checked')) values[input.name] = input.value
-//                             break
-//                         case 'file':
-//                             values[input.name] = input.files
-//                             break
-//                         default:
-//                             values[input.name] = input.value
-//                     }
-
-//                     return values
-//                 }, {})
-
-//                 _this.onSubmit(formValues)
-//             } else {
-
-//                 // formElement.submit()
-//                 console.log('Default');
-//             }
-//         }
-//     }
-// }
